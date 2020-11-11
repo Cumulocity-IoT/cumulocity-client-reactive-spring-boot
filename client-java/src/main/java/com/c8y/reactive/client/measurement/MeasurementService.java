b9786ac5e2c0bb2fd9bca8f5ee922e2a1d3cc4cf
Alexander Pester (APES <alexander.pester@softwareag.com>
2020-11-11 20:47:36 +0100

    add implementation of service methods

--- client-java/src/main/java/com/c8y/reactive/client/measurement/MeasurementService.java
+++ client-java/src/main/java/com/c8y/reactive/client/measurement/MeasurementService.java
@@ -3,8 +3,13 @@ package com.c8y.reactive.client.measurement;
 import java.util.List;
 import java.util.Map;
 
+import com.c8y.reactive.client.core.C8yWebClientFactory;
+import com.c8y.reactive.client.inventory.ManagedObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -14,25 +19,59 @@ import reactor.core.publisher.Mono;
 @Service
 public class MeasurementService {
 
+	private final WebClient webClient;
+
+	public MeasurementService(C8yWebClientFactory c8yWebClientFactory) {
+		webClient = c8yWebClientFactory.buildC8yWebClient();
+	}
+
 	/**
 	 * Gets the details of selected measurement.
 	 * 
-	 * @param id
-	 * @return
+	 * @param id measurement id, can be a string or a number
+	 * @return Measurement
 	 */
 	public Mono<Measurement> detail(Object id) {
-		return null;
+		Mono<Measurement> result = webClient.get().uri("/measurement/measurements/{id}", id).retrieve()
+				.bodyToMono(Measurement.class);
+		return result;
 	}
-	
+
+	/**
+	 * Gets the details of selected measurement and returns complete ResponseEntity.
+	 *
+	 * @param id measurement id, can be a string or a number
+	 * @return ResponseEntity
+	 */
+	public Mono<ResponseEntity<Measurement>> detailRE(Object id) {
+		Mono<ResponseEntity<Measurement>> result = webClient.get().uri("/measurement/measurements/{id}", id)
+				.exchange().flatMap(response -> response.toEntity(Measurement.class));
+		return result;
+	}
+
 	/**
 	 * Creates a new measurement.
 	 * 
 	 * @param measurement
-	 * @param sourceId
-	 * @return
+	 * @return Measurement
 	 */
-	public Mono<Measurement> create(Measurement measurement, Object sourceId) {
-		return null;
+	public Mono<Measurement> create(Measurement measurement) {
+		Mono<Measurement> result = webClient.post().uri("/measurement/measurements")
+				.accept(MediaType.APPLICATION_JSON).bodyValue(measurement).retrieve().bodyToMono(Measurement.class);
+		return result;
+	}
+
+	/**
+	 * Creates a new measurement.
+	 *
+	 * @param measurement
+	 * @return ResponseEntity
+	 */
+	public Mono<ResponseEntity<Measurement>> createRE(Measurement measurement) {
+		Mono<ResponseEntity<Measurement>> result = webClient.post().uri("/measurement/measurements")
+				.accept(MediaType.APPLICATION_JSON).bodyValue(measurement).exchange()
+				.flatMap(response -> response.toEntity(Measurement.class));
+		return result;
 	}
 	
 	/**
@@ -42,7 +81,9 @@ public class MeasurementService {
 	 * @return
 	 */
 	public Mono<Void> delete(Object id) {
-		return null;
+		Mono<Void> result = webClient.delete().uri("/measurement/measurements/{id}", id).retrieve()
+				.bodyToMono(Void.class);
+		return result;
 	}
 	
 	/**
